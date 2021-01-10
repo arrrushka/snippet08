f4a91fccfa3b757eebdfab53aac37e24cf9a2434
arrrushka <aruzhan2807@gmail.com>
2021-01-10 19:53:55 +0600

    lecture 2

--- main.go
+++ main.go
@@ -1,8 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+// handler
+func home(w http.ResponseWriter, r *http.Request)  {
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
+
+	w.Write([]byte("Hello Tleuzhan Mukatayev"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request)  {
+	id,err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w,r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a snippet with id %v", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Create a specific snippet..."))
+}
 
 func main()  {
-	fmt.Println("Hello Tluzhan Mukatayev")
+	// router
+	mux := http.NewServeMux()
+	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet",showSnippet)
+	mux.HandleFunc("/snippet/create",createSnippet)
 
+	//default server
+	err := http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
 }
