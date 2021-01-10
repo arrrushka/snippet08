d132ace31c15ddba4d75f3b2927180a51a93a53f
arrrushka <aruzhan2807@gmail.com>
2021-01-10 20:21:12 +0600

    2.5 the final version of create snippet function

--- main.go
+++ main.go
@@ -26,18 +26,20 @@ func showSnippet(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "Display a snippet with id %v", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Create a specific snippet..."))
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main()  {
-	// router
-	mux := http.NewServeMux()
-	mux.HandleFunc("/",home)
-	mux.HandleFunc("/snippet",showSnippet)
-	mux.HandleFunc("/snippet/create",createSnippet)
-
-	//default server
-	err := http.ListenAndServe(":4000",mux)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/snippet", showSnippet)
+	http.HandleFunc("/snippet/create", createSnippet)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", nil)
 	log.Fatal(err)
 }
